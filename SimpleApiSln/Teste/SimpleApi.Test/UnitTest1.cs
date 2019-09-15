cbcb2835edb3b97420328ac55257eb899517d860
professorthiagoxavier <professorthiagoxavier@gmail.com>
2019-09-15 17:13:28 -0300

    projeto de testes

---
+++ SimpleApiSln/Teste/SimpleApi.Test/UnitTest1.cs
@@ -0,0 +1,32 @@
+using Xunit;
+using SimpleApi.Controllers;
+using System.Linq;
+
+namespace SimpleApi.Test
+{
+    public class UnitTest1
+    {
+
+        ValuesController _controller = new ValuesController();
+
+        [Fact]
+        public void RetornarValores()
+        {
+            var returnValue = _controller.Get().Value.ToList();
+            Assert.True(returnValue.Count > 0);
+        }
+
+        [Fact]
+        public void RetornarValorPeloId()
+        {
+            var retorno = _controller.Get(1);
+            Assert.Equal(retorno.Value, "thiago");
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
